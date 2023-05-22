889a334ed5d336300d9693d17ac753c7c14a6f1a
yiyayamaya <wuxingjian98@gmail.com>
2023-05-22 11:25:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React11111
         </a>
       </header>
     </div>
